6758b2836acb38df20a5778908811e07815a0776
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2022-10-22 12:55:14 -0400

    more

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Angular13JwtAuth'),
+      dir: require('path').join(__dirname, './coverage/JuwanjiAngular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular13JwtAuth'`, () => {
+  it(`should have as title 'JuwanjiAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular13JwtAuth');
+    expect(app.title).toEqual('JuwanjiAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular13JwtAuth app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('JuwanjiAngular app is running!');
   });
 });
